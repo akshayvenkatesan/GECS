bd1a8c36c7ea8d0cac654b6e3522e02e26ee799f
Akshay <Akshay@Akshay>
2018-08-19 01:30:47 +0530

    Latest Commit

--- application/views/userdashboard.php
+++ application/views/userdashboard.php
@@ -44,7 +44,7 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
                     <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                        <a class="nav-link" href="userdashboard.php">
+                        <a class="nav-link" href="userdashboard">
                             <i class="fa fa-fw fa-dashboard"></i>
                             <span class="nav-link-text">Dashboard</span>
                         </a>
